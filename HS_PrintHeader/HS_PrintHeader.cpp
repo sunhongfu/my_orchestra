6050e24a5d485ad1966bee11d12ae65da5a94478
Hongfu Sun <sunhongfu@gmail.com>
2018-11-05 17:07:27 -0700

    print more header info such as psd name, series name etc.

--- HS_PrintHeader/HS_PrintHeader.cpp
+++ HS_PrintHeader/HS_PrintHeader.cpp
@@ -52,13 +52,30 @@ int main(const int argc, const char* const argv[])
     // Read POOL Header
     Legacy::PoolHeaderStruct header;
     pfileReader.ReadHeader(header);
-    
+    std::cout << "rdb_hdr_image.psdname is " << header.rdb_hdr_image.psdname << std::endl;
+    std::cout << "rdb_hdr_image.psd_iname is " << header.rdb_hdr_image.psd_iname << std::endl;
+    std::cout << "rdb_hdr_image.psdnameannot is " << header.rdb_hdr_image.psdnameannot << std::endl;
+
+//    std::cout << "rdb_hdr_image.proj_name is " << header.rdb_hdr_image.proj_name << std::endl;
+    std::cout << "rdb_hdr_image.cname is " << header.rdb_hdr_image.cname << std::endl;
+    std::cout << "rdb_hdr_image.GEcname is " << header.rdb_hdr_image.GEcname << std::endl;
+    std::cout << "rdb_hdr_series.prtcl is " << header.rdb_hdr_series.prtcl << std::endl;
+    std::cout << "rdb_hdr_series.se_desc is " << header.rdb_hdr_series.se_desc << std::endl;
+
+
     std::cout << "rdb_hdr_rec.rdb_hdr_asset is " << header.rdb_hdr_rec.rdb_hdr_asset << std::endl;
     std::cout << "rdb_hdr_rec.rdb_hdr_asset_R is " << header.rdb_hdr_rec.rdb_hdr_asset_R << std::endl;
     std::cout << "rdb_hdr_rec.rdb_hdr_asset_slice_R is " << header.rdb_hdr_rec.rdb_hdr_asset_slice_R << std::endl;
     std::cout << "rdb_hdr_rec.rdb_hdr_image.ti is " << header.rdb_hdr_image.ti << std::endl;
+    std::cout << "rdb_hdr_rec.rdb_hdr_kacq_uid is " << header.rdb_hdr_rec.rdb_hdr_kacq_uid << std::endl; // kacq uid, e.g. kacq_yz.txt.1104145905
+    std::cout << "rdb_hdr_rec.rdb_hdr_user16 is " << header.rdb_hdr_rec.rdb_hdr_user16 << std::endl;
+    // this is the uniPolar_flag, 1 means uni, otherwiese bi, for the efgre3d based sequences
+    
+    
+    
+    
 
-
+    
     return 0;
     
 }
